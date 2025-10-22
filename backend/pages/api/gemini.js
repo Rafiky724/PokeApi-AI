1fc9c1da1ace838d6f7e6da5d701a9bc3079ab9f
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-10-21 23:18:02 -0500

    first commit

--- backend/pages/api/gemini.js
+++ backend/pages/api/gemini.js
@@ -1,11 +1,10 @@
-import { GoogleGenAI } from '@google/genai';
-import rateLimiter from '../../lib/rateLimiter'; // importa el middleware
-import { corsApiPokemon } from '@/lib/cors-middleware';
+import { GoogleGenAI } from "@google/genai";
+import rateLimiter from "../../lib/rateLimiter"; // importa el middleware
+import { corsApiPokemon } from "@/lib/cors-middleware";
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export default async function handler(req, res) {
-  
   await corsApiPokemon(req, res);
 
   // try {
@@ -15,24 +14,28 @@ export default async function handler(req, res) {
   //   return res.status(429).json({ message: 'Has alcanzado el límite de 3 solicitudes por día.' });
   // }
 
-  if (req.method !== 'POST') {
-    res.setHeader('Allow', ['POST']);
-    return res.status(405).json({ message: 'Método no permitido' });
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  if (req.method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    return res.status(405).json({ message: "Método no permitido" });
   }
 
   const { pokemon } = req.body;
 
-  if (!pokemon || typeof pokemon !== 'string') {
+  if (!pokemon || typeof pokemon !== "string") {
     return res
       .status(400)
-      .json({ message: 'Falta el nombre del Pokémon o es inválido' });
+      .json({ message: "Falta el nombre del Pokémon o es inválido" });
   }
 
   const prompt = `Dame un dato curioso, educativo o entretenido sobre el Pokémon ${pokemon}. Solo debes darme el dato curioso, evita información innecesaria`;
 
   try {
     const gResponse = await ai.models.generateContent({
-      model: 'gemini-2.5-flash',
+      model: "gemini-2.5-flash",
       contents: prompt,
     });
 
@@ -41,12 +44,14 @@ export default async function handler(req, res) {
     if (!text) {
       return res
         .status(500)
-        .json({ message: 'No se pudo obtener un dato curioso' });
+        .json({ message: "No se pudo obtener un dato curioso" });
     }
 
     return res.status(200).json({ datoCurioso: text });
   } catch (error) {
-    console.error('Error en POST:', error);
-    return res.status(500).json({ message: 'Error al generar contenido con Gemini' });
+    console.error("Error en POST:", error);
+    return res
+      .status(500)
+      .json({ message: "Error al generar contenido con Gemini" });
   }
 }
