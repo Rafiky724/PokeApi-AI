2206cb3321ac6ee4da4b91e144ecfb9653b70f55
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-10-21 22:48:43 -0500

    first commit

--- backend/lib/cors-middleware.js
+++ backend/lib/cors-middleware.js
@@ -3,7 +3,7 @@ import { initMiddleware } from './init-middleware';
 
 export const corsApiPokemon = initMiddleware(
   Cors({
-    origin: ['http://localhost:3000', 'https://tudominio.com'], // Cambia esto
+    origin: ['http://localhost:3000', 'https://pokeapi-ai.netlify.app/'], // Cambia esto
     methods: ['GET', 'POST', 'OPTIONS'],
     credentials: true,
   })
